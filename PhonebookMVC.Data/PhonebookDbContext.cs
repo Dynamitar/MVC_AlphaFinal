e8887a8d43465b3d64855448fa066829ddf0af25
Dimitar Gochev <dimitarggochev@gmail.com>
2018-01-30 21:01:41 +0200

    Add files via upload

---
+++ PhonebookMVC.Data/PhonebookDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using PhonebookMVC.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhonebookMVC.Data
+{
+    public class PhonebookDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public PhonebookDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+
+        }
+
+        public static PhonebookDbContext Create()
+        {
+            return new PhonebookDbContext();
+        }
+
+
+
+
+    }
+}
